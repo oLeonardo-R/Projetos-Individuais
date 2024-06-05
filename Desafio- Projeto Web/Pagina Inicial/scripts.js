865dad649166111fdeaee563742db0d8b69c9f9d
uLeonardoR <leo.rocha1@hotmail.com>
2024-06-04 23:21:30 -0300

    Entrega 2-Parte

---
+++ Desafio- Projeto Web/Pagina Inicial/scripts.js
@@ -0,0 +1,12 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("fadeInUp");
+      observer.unobserve(entry.target);
+    }
+  });
+});
+
+document.querySelectorAll(".recursos-conteudo, .recurso").forEach((element) => {
+  observer.observe(element);
+});
